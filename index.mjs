fa63aa61a61bb631c1a68d01345ecd752c8db4e1
adamkudz <adam.kudzin@gmail.com>
2024-01-08 22:01:41 -0500

    Delete unused reference links and update uploadToSupabase function

--- index.mjs
+++ index.mjs
@@ -3,19 +3,19 @@ import { extractVehicles } from "./lib/extractVehicles.mjs";
 import { scrapeReferenceLinks } from "./lib/scrapeReferenceLinks.mjs";
 import { downloadReferenceFiles } from "./lib/downloadReferenceFiles.mjs";
 import { getDatabaseAccidentIds } from "./lib/getDatabaseAccidentIds.mjs";
-import { getAccidentIds } from "./lib/getAccidentIds.mjs";
+import { getScrapedAccidentIds } from "./lib/getScrapedAccidentIds.mjs";
 import { makeAccidentMap } from "./lib/makeAccidentMap.mjs";
 import { uploadToSupabase } from "./lib/uploadToSupabase.mjs";
 import { checkForNewAccidents } from "./lib/checkForNewAccidents.mjs";
 
 import * as fspromises from "fs/promises";
-import { get } from "http";
 
 const ntsbUrl = "https://www.ntsb.gov/Pages/AviationQueryv2.aspx";
 const referenceFolder = "reference-links";
 const rawFolder = "raw-cases";
 const rawFileName = "tbm-raw-cases.json";
 const accidentFolder = "accidents";
+const dbName = "tbm-accidents";
 const config = {
     date: "01/01/1992",
     model: "tbm",
@@ -25,15 +25,6 @@ const config = {
 async function deleteDirectories(rawFolder, referenceFolder, accidentFolder) {
     console.log("deleting directories");
     try {
-        if (
-            (await fspromises.readdir(process.cwd() + `/${referenceFolder}`))
-                .length > 0
-        ) {
-            console.log("reference folder exists...deleting");
-            await fspromises.rm(process.cwd() + `/${referenceFolder}`, {
-                recursive: true,
-            });
-        }
         if (
             (
                 await fspromises.readFile(
@@ -62,34 +53,44 @@ async function deleteDirectories(rawFolder, referenceFolder, accidentFolder) {
 }
 
 async function getNewAccidents() {
-    let { newAccidents, scrapedAccidentIds } = await checkForNewAccidents(
-        ntsbUrl,
-        config,
-        rawFolder,
-        rawFileName
-    );
+    try {
+        let { newAccidents, scrapedAccidentIds } = await checkForNewAccidents(
+            ntsbUrl,
+            config,
+            rawFolder,
+            rawFileName,
+            dbName
+        );
 
-    if (newAccidents.length > 0) {
-        let supabaseResponse = await uploadToSupabase(newAccidents);
-        console.log("supabase response", supabaseResponse);
-    } else {
-        console.log("No new accidents to upload");
+        if (newAccidents.length > 0) {
+            console.log(newAccidents.length + " new accidents found");
+            let supabaseResponse = await uploadToSupabase(newAccidents, dbName);
+            console.log(supabaseResponse.length + " new accidents uploaded");
+            await fspromises.rm(process.cwd() + `/${rawFolder}`, {
+                recursive: true,
+            });
+        } else {
+            console.log("No new accidents to upload");
+            await fspromises.rm(process.cwd() + `/${rawFolder}`, {
+                recursive: true,
+            });
+        }
+    } catch (err) {
+        console.error(err);
     }
 }
 
-await deleteDirectories(rawFolder, referenceFolder, accidentFolder);
+await getNewAccidents();
 
-// await getNewAccidents();
+// await fspromises.mkdir(`${referenceFolder}`, { recursive: true });
 
-await fspromises.mkdir(`${referenceFolder}`, { recursive: true });
+// await scrapeRawData(ntsbUrl, config, rawFolder, rawFileName);
 
-await scrapeRawData(ntsbUrl, config, rawFolder, rawFileName);
+// await extractVehicles(rawFolder);
 
-await extractVehicles(rawFolder);
+// let accidentIds = await getAccidentIds(rawFolder, rawFileName);
 
-let accidentIds = await getAccidentIds(rawFolder, rawFileName);
-
-await scrapeReferenceLinks(accidentIds, referenceFolder);
+// await scrapeReferenceLinks(accidentIds, referenceFolder);
 
 // let accidentMap = await makeAccidentMap(`${referenceFolder}`);
 
